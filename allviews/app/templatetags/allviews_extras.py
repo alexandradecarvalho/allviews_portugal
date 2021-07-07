fd59850737f776f42301584e386afedbb3507554
Alexa <alexandracarvalho@ua.pt>
2021-07-07 12:24:52 +0100

    Made filtering work by creating a custom filter that removes spacing in the strings which was creating problems

--- allviews/app/templatetags/allviews_extras.py
+++ allviews/app/templatetags/allviews_extras.py
@@ -6,3 +6,11 @@ register = template.Library()
 def addstr(arg1, arg2):
     """concatenate arg1 & arg2"""
     return str(arg1) + str(arg2)
+
+@register.filter
+def mystrip(str1, str2):
+    result = ""
+    for character in str1:
+        if character != " ":
+            result += character
+    return result
